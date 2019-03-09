6eb960efff1a36ee8a83a42eb41bc1737514ab6e
Mo Ramezanpoor <me@mohsenr.com>
2019-03-09 16:31:27 +0000

    Solve int-Nat.

--- exercises/int-Nat.rkt
+++ exercises/int-Nat.rkt
@@ -19,6 +19,25 @@
 ;; Define a function called zip that takes an argument of type (Vec A n) and a
 ;; second argument of type (Vec B n) and evaluates to a vlue of type (Vec (Pair A B) n),
 ;; the result of zipping the first and second arguments.
+        
+(claim zip
+  (Pi ([A U] [B U] [n Nat])
+    (-> (Vec A n) (Vec B n) (Vec (Pair A B) n))))
+(define zip
+  (lambda (A B n)
+    (ind-Nat n
+      (lambda (k) (-> (Vec A k) (Vec B k) (Vec (Pair A B) k)))
+      (lambda (_ _) vecnil)
+      (lambda (k zip-k)
+        (lambda (es fs)
+          (vec:: (cons (head es) (head fs))
+            (zip-k (tail es) (tail fs))))))))
+
+(check-same (Vec (Pair Nat Atom) 2)
+            (zip Nat Atom 2
+                 (vec:: 1 (vec:: 2 vecnil))
+                 (vec:: 'orange (vec:: 'pear vecnil)))
+            (vec:: (cons 1 'orange) (vec:: (cons 2 'pear) vecnil)))
 
 ;; Exercise 7.1
 ;;
@@ -32,6 +51,22 @@
            [m Nat])
           (-> (Vec E n) (Vec E m)
               (Vec E (+ n m)))))
+(define append
+  (lambda (E n m)
+    (ind-Nat n
+      (lambda (k) (-> (Vec E k) (Vec E m) (Vec E (+ k m))))
+      (lambda (_ fs) fs)
+      (lambda (k append-k)
+        (lambda (es fs)
+          (vec:: (head es)
+            (append-k (tail es) fs)))))))
+
+(check-same (Vec Nat 5)
+            (append Nat 2 3
+                    (vec:: 0 (vec:: 1 vecnil))
+                    (vec:: 2 (vec:: 3 (vec:: 4 vecnil))))
+            (vec:: 0 (vec:: 1 (vec:: 2 (vec:: 3 (vec:: 4 vecnil))))))
+
 
 ;; Exercise 7.2
 ;;
@@ -41,3 +76,20 @@
 ;;
 ;; NB: The type of the function should guarantee that we can't drop more elements
 ;; than there are in the first argument.
+
+(claim drop-last-k
+  (Pi ([E U] [k Nat] [n Nat])
+    (-> (Vec E (+ n k)) (Vec E n))))
+(define drop-last-k
+  (lambda (E k n)
+    (ind-Nat n
+      (lambda (n) (-> (Vec E (+ n k)) (Vec E n)))
+      (lambda (_) vecnil)
+      (lambda (k drop-last-k-1)
+        (lambda (es)
+          (vec:: (head es) (drop-last-k-1 (tail es))))))))
+
+(check-same (Vec Nat 1)
+            (drop-last-k Nat 2 1
+                         (vec:: 0 (vec:: 1 (vec:: 2 vecnil))))
+            (vec:: 0 vecnil))
